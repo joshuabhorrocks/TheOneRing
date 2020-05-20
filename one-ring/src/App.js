23c34c82351f4c6b8b944dc85a80aff9ef39bc23
Joshua Horrocks <joshuabhorrocks@gmail.com>
2020-05-20 17:21:30 -0600

    Created initial navbar and components

--- one-ring/src/App.js
+++ one-ring/src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import './App.css';
+
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Rules from "./components/Home";
+import Announcements from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Torlotr.com
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/rules" component={Rules} exact />
+          <Route path="/announcements" component={Announcements} exact />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
